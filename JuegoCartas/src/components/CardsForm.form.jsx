e72c81e53844d5a9bcb21a7ccd0b1b1213ed2063
DiamondStalker <cmoreno981@gmail.com>
2022-12-11 18:27:14 -0500

    Functionality is improved and new functions and validations are added.

--- JuegoCartas/src/components/CardsForm.form.jsx
+++ JuegoCartas/src/components/CardsForm.form.jsx
@@ -6,7 +6,7 @@ const specialLetters = {
 }
 
 const specialNumbers = {
-    "1" : "A",
+    "1": "A",
     "11": "J",
     "12": "Q",
     "13": "K"
@@ -123,11 +123,8 @@ export async function validateCollectCards(player) {
     Object.keys(tempInfo.listCards).forEach((temp) => {
 
         if (tempInfo.listCards[temp].length === 3) {
-
-
-            if (temp.search(/\d/gim) > -1) {// Validation for a third or fourth by letter value
-                console.log('Se encontro una terna')
-
+            /* ------------ Validation for a third or fourth by letter value ------------ */
+            if (temp.search(/\d/gim) > -1) {
                 if (Object.keys(tempInfo.threesomes1).length === 0 && Object.keys(tempInfo.threesomes2).shift() !== temp) {
                     tempInfo.threesomes1[`${temp}`] = tempInfo.listCards[temp];
                 } else if (Object.keys(tempInfo.threesomes2).length === 0 && Object.keys(tempInfo.threesomes1).shift() !== temp) {
@@ -137,28 +134,42 @@ export async function validateCollectCards(player) {
         }
         /* ------------- Validacion cuando hay mas de 4 cartas posibles ------------- */
         else if (tempInfo.listCards[temp].length > 3) {
-            if (temp.search(/\d/gim) > -1) {// Validation for a third or fourth by letter value
-                console.log('Se encontro una cuarta')
-
+            /* ------------ Validation for a third by letter value ------------ */
+            if (temp.search(/\d/gim) > -1) {
                 if (Object.keys(tempInfo.quartet).length === 0) {
                     tempInfo.quartet[`${temp}`] = tempInfo.listCards[temp];
-
-                    console.log('VaKeylor de terna 1')
-                    console.log(Object.keys(tempInfo.threesomes1).shift())
-
-                    console.log('Key de terna 2')
-                    console.log(Object.keys(tempInfo.threesomes2).shift())
-
-                    console.log('Key actual')
-                    console.log(temp)
                     /* ----- En caso de encontrar una cuarta validamos la ternas anteriores ----- */
                     /* ------------ en caso de que coincida una, reiniciamos su valor ----------- */
                     if (Object.keys(tempInfo.threesomes1).shift() === temp) {
-                        console.log('Reiniciamos el valor de la terna 1')
                         delete tempInfo.threesomes1[`${temp}`]
                     }
                     if (Object.keys(tempInfo.threesomes2).shift() === temp) {
-                        console.log('Reiniciamos el valor de la terna 2')
+                        delete tempInfo.threesomes2[`${temp}`]
+                    }
+                }
+            }
+        }
+        /* -------------------- Validation of the fourth by pint -------------------- */
+        const h = validationBySuit(temp, tempInfo.listCards[temp])
+
+
+        if (Object.keys(h).length != 0) {
+            if (h[Object.keys(h).shift()].length == 3) {
+                if (Object.keys(tempInfo.threesomes1).length === 0 && Object.keys(tempInfo.threesomes2).shift() !== temp) {
+                    tempInfo.threesomes1[`${temp}`] = h[Object.keys(h).shift()];
+                } else if (Object.keys(tempInfo.threesomes2).length === 0 && Object.keys(tempInfo.threesomes1).shift() !== temp) {
+                    tempInfo.threesomes2[`${temp}`] = h[Object.keys(h).shift()];
+                }
+            }
+            else if (h[Object.keys(h).shift()].length == 4) {
+                if (Object.keys(tempInfo.quartet).length === 0) {
+                    tempInfo.quartet[`${temp}`] = h[Object.keys(h).shift()];
+                    /* ----- En caso de encontrar una cuarta validamos la ternas anteriores ----- */
+                    /* ------------ en caso de que coincida una, reiniciamos su valor ----------- */
+                    if (Object.keys(tempInfo.threesomes1).shift() === temp) {
+                        delete tempInfo.threesomes1[`${temp}`]
+                    }
+                    if (Object.keys(tempInfo.threesomes2).shift() === temp) {
                         delete tempInfo.threesomes2[`${temp}`]
                     }
                 }
@@ -169,22 +180,45 @@ export async function validateCollectCards(player) {
     return tempInfo
 }
 /**
- * 
+ * Function allowing to create the url structure of the charts
  * @param {Object} list of winning cards
  * @returns String with the html of the calculated images
  */
 export function calculateUrlImg(list) {
-
     const key = Object.keys(list).shift()
-
     let text = "";
-
     list[key].forEach(x => {
-        if (key.search(/d/gmi) == -1) {
-            text += `<img src="https://deckofcardsapi.com/static/img/${(() => { return key === "1"||key === "11"|key === "12" ? specialNumbers[key] : key})()}${x.charAt(0)}.png" alt="Girl in a jacket" height="50">  \t   `
+        if (key.search(/\d/gmi) > -1) {
+            console.log('Hay numeros')
+            text += `<img src="https://deckofcardsapi.com/static/img/${(() => { return key === "1" || key === "11" | key === "12" | key === "13" ? specialNumbers[key] : key.replace('10', '0').replace('13','K') })()}${x.charAt(0)}.png" alt="Girl in a jacket" height="100">  \t   `
+        } else {
+            console.log('Son letras')
+            text += `<img src="https://deckofcardsapi.com/static/img/${(() => { return x === "1" || x === "11" | x === "12" | key === "13" ? specialNumbers[x] : x.replace('10', '0').replace('13','K') })()}${key.charAt(0)}.png" alt="Girl in a jacket" height="100">  \t   `
         }
     })
-
     return text;
-
 }
+
+export function validationBySuit(key, list) {
+    let isLadder = false
+    const ladder = {}
+
+    for (let y = 0; y < list.length; y++) {
+        if (isLadder) continue
+        else {
+            if (list[y] == (list[y + 1] - 1) && list[y] == (list[y + 2] - 2) && list[y] == (list[y + 3] - 3)) {
+                isLadder = true;
+                ladder[`${key}`] = [list[y], list[y + 1], list[y + 2], list[y + 3]]
+            }
+            else if (list[y] == (list[y + 1] - 1) && list[y] == (list[y + 2] - 2)) {
+                isLadder = true;
+                ladder[`${key}`] = [list[y], list[y + 1], list[y + 2]]
+            }
+        }
+    }
+    if (isLadder) {
+        console.log('Valor dentro de la funcion')
+        console.log(ladder)
+    }
+    return ladder
+} 
--- JuegoCartas/src/context/GameProvider.jsx
+++ JuegoCartas/src/context/GameProvider.jsx
@@ -3,7 +3,7 @@ import DeckOfCardsAPI from '../services/deckofcardsapi';
 import GameContext from './GameContext';
 import Swal from 'sweetalert2';
 import 'animate.css';
-import { sortCards, validateCollectCards , calculateUrlImg } from '../components/CardsForm.form';
+import { sortCards, validateCollectCards, calculateUrlImg } from '../components/CardsForm.form';
 
 const GameProvider = ({ children }) => {
 	const [idGame, setIdGame] = useState(null);
@@ -117,26 +117,33 @@ const GameProvider = ({ children }) => {
 			const cards = await DeckOfCardsAPI.getCards(idGame);
 
 			if (playerOne.currentCards === 10) {
-				console.log(
-					'Los jugadores ya tienen 10 cartas y no pueden tener mas de 10'
+				// console.clear();
+				// const style = 'font-weight: bold; font-size: 50px;color: red; text-shadow: 3px 3px 0 rgb(217,31,38) , 6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8) , 12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgb(2,135,206) , 18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113)';
+				// console.log('%c DiamondStalker!', style);
+				console.warn(
+					'Players already have 10 cards and may not have more than 10.'
 				);
+
+				/* --- We have to validate which letter is not necessary to be eliminated. -- */
+
+
 			}
-
-			setPlayerOne({
-				...playerOne,
-				cards: [...playerOne.cards, cards[0]],
-				currentCards: (() => {
-					return playerOne.currentCards + 1;
-				})(),
-			});
-			setPlayerTwo({
-				...playerTwo,
-				cards: [...playerTwo.cards, cards[1]],
-				currentCards: (() => {
-					return playerTwo.currentCards + 1;
-				})(),
-			});
-
+			
+				setPlayerOne({
+					...playerOne,
+					cards: [...playerOne.cards, cards[0]],
+					currentCards: (() => {
+						return playerOne.currentCards + 1;
+					})(),
+				});
+				setPlayerTwo({
+					...playerTwo,
+					cards: [...playerTwo.cards, cards[1]],
+					currentCards: (() => {
+						return playerTwo.currentCards + 1;
+					})(),
+				});
+			
 
 			await sortCards(playerOne, cards[0]);
 			await sortCards(playerTwo, cards[1]);
@@ -153,9 +160,8 @@ const GameProvider = ({ children }) => {
 
 				Swal.fire({
 					icon: 'success',
-					title: 'Winner',
-					text: `El jugador ${playerOne.name} es el ganador`,
-					html : `<h1>Player name ${playerOne.name}</h1> <hr>threesomes1 ${calculateUrlImg(playerOne.threesomes1)} <br> threesomes2 ${calculateUrlImg(playerOne.threesomes2)} <br> quartet ${calculateUrlImg(playerOne.quartet)} <br>`,
+					title: 'Player 1 has won',
+					html: `<h1>Player name ${playerOne.name}</h1> <hr>threesomes1 ${calculateUrlImg(playerOne.threesomes1)} <br><br><br> threesomes2 ${calculateUrlImg(playerOne.threesomes2)} <br><br><br> quartet ${calculateUrlImg(playerOne.quartet)} <br><br><br>`,
 					showConfirmButton: true,
 					timer: 10000,
 					showClass: {
@@ -170,16 +176,15 @@ const GameProvider = ({ children }) => {
 					}
 				});
 
-			}else if (Object.keys(playerTwo.threesomes1).length !== 0 && Object.keys(playerTwo.threesomes2).length !== 0 && Object.keys(playerTwo.quartet).length !== 0) {
+			} else if (Object.keys(playerTwo.threesomes1).length !== 0 && Object.keys(playerTwo.threesomes2).length !== 0 && Object.keys(playerTwo.quartet).length !== 0) {
 				setWin(true);
 				setShowToast(true);
 				setWinName(playerTwo.name);
 
 				Swal.fire({
 					icon: 'success',
-					title: 'Winner',
-					text: `<h1>Player name ${playerTwo.name}</h1> <hr>El jugador ${playerTwo.name} es el ganador <br> jajaj`,
-					html : ` threesomes1 ${calculateUrlImg(playerTwo.threesomes1)} <br> threesomes2 ${calculateUrlImg(playerTwo.threesomes2)} <br> quartet ${calculateUrlImg(playerTwo.quartet)} <br>`,
+					title: 'Player 2 has won',
+					html: `<h1>Player name ${playerOne.name}</h1> <hr>threesomes1 ${calculateUrlImg(playerTwo.threesomes1)} <br><br><br> threesomes2 ${calculateUrlImg(playerTwo.threesomes2)} <br><br><br> quartet ${calculateUrlImg(playerTwo.quartet)} <br><br><br>`,
 					showConfirmButton: true,
 					timer: 10000,
 					showClass: {
